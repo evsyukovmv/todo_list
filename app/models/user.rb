f009a4b6cdcd1e17e5a546275d3009043a56c666
Mihail Evsyukov <evsyukovmv@gmail.com>
2012-02-04 17:43:32 +0200

    pages signin, sigout; top right links

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :name, :presence => true,
+  validates :name,  :presence => true,
                     :length => { :maximum => 50 }
   validates :email, :presence => true,
                     :format => { :with => email_regex },
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,72 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = {
+        :name => "Example User",
+        :email => "user@example.com",
+        :password => "foobar",
+        :password_confirmation => "foobar"
+    }
+  end
+
+  it "should create user with valid attributes" do
+    User.create!(@attr)
+  end
+
+  it "should require a name" do
+    no_name_user = User.new(@attr.merge(:name => ""))
+    no_name_user.should_not be_valid
+  end
+
+  it "should have maximum length 50 of name" do
+    long_name_user = User.new(@attr.merge(:name => "n"*51))
+    long_name_user.should_not be_valid
+  end
+
+  it "should require an email address" do
+    no_email_user = User.new(@attr.merge(:email => ""))
+    no_email_user.should_not be_valid
+  end
+
+  it "should require a password" do
+    no_password_user = User.new(@attr.merge(:password => ""))
+    no_password_user.should_not be_valid
+  end
+
+  it "should require valid length of password between 6 and 40" do
+    short_password_user = User.new(@attr.merge(:password=> "p"*5, :password_confirmation => "1"*5))
+    short_password_user.should_not be_valid
+
+    long_password_user = User.new(@attr.merge(:password => "p"*41, :password_confirmation => "p"*41))
+    long_password_user.should_not be_valid
+  end
+
+  it "should require a password_confirmation" do
+    no_password_confirmation_user = User.new(@attr.merge(:password_confirmation=> "wrong_password"))
+    no_password_confirmation_user.should_not be_valid
+  end
+
+  it "should not accept wrong email" do
+    wrong_emails = %w[mail.com mail@mail @mail.com mail@mail. mail@.com]
+    wrong_emails.each do |email|
+      wrong_email_user = User.new(@attr.merge(:email => email))
+      wrong_email_user.should_not be_valid
+    end
+  end
+
+  it "should accept valid email" do
+    valid_emails = %w[mail@mail.com some@mail.ru mail@gmail.com post@i.ua]
+    valid_emails.each do |email|
+      valid_email_user = User.new(@attr.merge(:email => email))
+      valid_email_user.should be_valid
+    end
+  end
+
+  it "should not accept two identical emails in different case" do
+    User.create(@attr.merge(:email => :email.upcase))
+    identical_email_user = User.new(@attr.merge(:email => :email.downcase))
+    identical_email_user.should_not be_valid
+  end
+
 end
